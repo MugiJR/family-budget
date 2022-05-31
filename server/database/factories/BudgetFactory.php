fe0d2cf9e777e24c03b86a1e7d11b64025cc1ffb
MugiJR <immugi@yahoo.com>
2022-05-31 17:57:49 +0200

    Included db file and budget model with facker

---
+++ server/database/factories/BudgetFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Budget>
+ */
+class BudgetFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->word(),
+            'amount' => $this->faker->randomDigit(),
+            'category' => $this->faker->word(),
+        ];
+    }
+}
